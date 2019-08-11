1f5695fd609757687bb850e1d5ab834b11256441
fromtenroku <kazuhironakanishi@icloud.com>
2019-08-11 20:27:09 +0900

    インクリ実装
    search.jsの編集

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,5 +1,39 @@
 $(function() {
-  $(".chat-group-form__input").on("keyup", function() {
-    var input = $(".chat-group-form__input").val();
+
+  var search_list = $("#user-search-result");
+
+  function appendUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${ user.name }</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+                </div>`
+    search_list.append(html);
+  }
+
+  $("#user-search-field").on("keyup", function() {
+    var input = $("#user-search-field").val();
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+    .done(function(users) {
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendErrMsgToHTML("一致するユーザーはいません");
+      }
+    })
+    .fail(function() {
+      alert('ユーザー検索に失敗しました');
+    })
   });
 });
+
