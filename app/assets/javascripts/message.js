aa7062cd390a18b200f6a0b1f6f289c6a89d3aef
fromtenroku <kazuhironakanishi@icloud.com>
2019-08-15 17:05:46 +0900

    4.追加したアクションをリクエストするよう実装しよう
    message.jsの編集

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -41,4 +41,24 @@ $(function(){
       alert('error');
     })
   })
+
+    var reloadMessages = function() {
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      last_message_id = $('.form__message').last().data('id');
+      $.ajax({
+        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+        url: 'api/messages',
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        console.log('success');
+      })
+      .fail(function() {
+        console.log('error');
+    });
+  };
 })
